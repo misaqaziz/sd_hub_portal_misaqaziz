2b08826c43ed6d5193876e7857840a1f20fc55d3
misaqaziz <mdmisaqaziz@gmail.com>
2024-10-07 13:39:50 +0530

    .

---
+++ sdhub-portal/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'signin', pathMatch: "full" },
+  { path: 'signin', component: SigninComponent },
+  { path: 'register', component: RegisterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
